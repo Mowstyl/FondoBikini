642440a0343781170bae789833e220c7bd0c3ec4
Mowstyl <indikalesken@hotmail.com>
2015-04-13 18:47:40 +0200

    Upload

---
+++ FondoBikini/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FondoBikini
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            logOut.IsEnabled = false;
+        }
+
+        private void iniciarSesion_Click(object sender, RoutedEventArgs e)
+        {
+            LoginWindow loginWindow = new LoginWindow();
+            loginWindow.ShowDialog();
+            foreach (userPassword userpass in userPasswords )
+            if (userpass.checkUser(loginWindow.user.Text)&&userpass.checkPass(loginWindow.password.Password))
+            {
+                loginWindow.Close();
+                MenuWindow menu = new MenuWindow();
+                this.Close();
+                menu.Show();
+            }
+            else
+            {
+                MessageBox.Show("Usuario o Clave incorrectos");
+            }
+            
+        }
+
+        private void cerrar_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        public class userPassword
+        {
+            private string user;
+            private string password;
+            public userPassword(string user, string password)
+            {
+                this.user = user;
+                this.password = password;
+            }
+            public bool checkUser(string user)
+            {
+                return this.user==user;
+            }
+            public bool checkPass(string password)
+            {
+                return this.password==password;
+            }
+        }
+
+        private userPassword[] userPasswords = { new userPassword("Gato", "Ninja") };
+    }
+}
